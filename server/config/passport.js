6ec9dcd1e676e571894c8e2263271d7c51476390
Livan Kumar <132928410+livan116@users.noreply.github.com>
2025-06-21 13:54:27 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -22,9 +22,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.NODE_ENV === 'production'
-        ? 'https://bluc-payed.vercel.app/api/auth/google/callback'
-        : 'http://localhost:3000/api/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
